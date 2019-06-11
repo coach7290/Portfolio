43932ca7aa53bc63f5bec00f12b00f8b68ed9a3e
coach7290 <coach7290@gmail.com>
2019-06-11 21:48:36 +0100

    Updates

---
+++ precache-manifest.a780e973ccfee25d303c0621d25bcad9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d35a23333021ae3f1f478243f03708",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "860b696d1534d18a2fb8",
+    "url": "/portfolio/static/css/main.d12a7d4f.chunk.css"
+  },
+  {
+    "revision": "1b245ae9accced17d2eb",
+    "url": "/portfolio/static/js/2.021e39fd.chunk.js"
+  },
+  {
+    "revision": "860b696d1534d18a2fb8",
+    "url": "/portfolio/static/js/main.a3d4818b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "a39e26c21234794e4a3d370435e27e8a",
+    "url": "/portfolio/static/media/EXXON.a39e26c2.PNG"
+  },
+  {
+    "revision": "8fb3fdabdca6fae9fe57a6617ec1f660",
+    "url": "/portfolio/static/media/Nike.8fb3fdab.PNG"
+  },
+  {
+    "revision": "27b39950acdaf8051bb0c94ba2a7512e",
+    "url": "/portfolio/static/media/Sap1.27b39950.PNG"
+  },
+  {
+    "revision": "7f6425d0277478a90c7d2b85a29f75de",
+    "url": "/portfolio/static/media/Siemens.7f6425d0.PNG"
+  },
+  {
+    "revision": "dd4493061b9bdf15a769e8562d78bfce",
+    "url": "/portfolio/static/media/Workforce.dd449306.PNG"
+  },
+  {
+    "revision": "281d84ca7fa2902e4399e887a8ab3305",
+    "url": "/portfolio/static/media/bb.281d84ca.png"
+  },
+  {
+    "revision": "b47c165aca5f280a12f7258a0bc88350",
+    "url": "/portfolio/static/media/client.b47c165a.jpg"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "b68bd4df745abb8d8538babb5626155b",
+    "url": "/portfolio/static/media/fermo.b68bd4df.jpg"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "cfc827abd3522241390d94435cf964fa",
+    "url": "/portfolio/static/media/marketing.cfc827ab.PNG"
+  },
+  {
+    "revision": "52c43fe4ca58895d05c58a4829a491b7",
+    "url": "/portfolio/static/media/oracle.52c43fe4.PNG"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/portfolio/static/media/twitter_icon.512536cb.png"
+  },
+  {
+    "revision": "9efd5a1dc2aff25703301ecaeab80014",
+    "url": "/portfolio/static/media/website.9efd5a1d.PNG"
+  }
+]);
